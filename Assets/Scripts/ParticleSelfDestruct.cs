16e427c972a4ec14a33cf4283a76eda33606a4bd
satsujhin <113123504+satsujhin@users.noreply.github.com>
2022-09-11 14:01:43 +0200

    blood

--- Assets/Scripts/ParticleSelfDestruct.cs
+++ Assets/Scripts/ParticleSelfDestruct.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class ParticleSelfDestruct : MonoBehaviour
 {
+
+    [SerializeField]
+    private List<AudioSource> sounds;
     private void Start()
     {
         StartCoroutine(DestroyAfterTime());
+        
     }
 
     private IEnumerator DestroyAfterTime()
